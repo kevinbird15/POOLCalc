87a928041f31f0f98a9ead483f024c2b0015012f
kevinbird15 <kevinbird15@gmail.com>
2018-03-03 21:54:08 -0600

    adding some historical and fixing some formatting

--- calculator/views.py
+++ calculator/views.py
@@ -1,7 +1,7 @@
 import urllib
 import urllib.request
 import json
-#from urllib.request import urlopen
+import pandas as pd
 from django.shortcuts import render, redirect
 from .forms import PayoutForm, CalculatorForm
 from .models import PayoutAmount, Calculator
@@ -35,6 +35,7 @@ def calculator_results(request):
 
 
 def enter_payout(request):
+    # TODO add USDETH price to payout model
     if request.method == "POST":
         form = PayoutForm(request.POST)
         if form.is_valid():
@@ -52,3 +53,30 @@ def about(request):
 
 def contact(request):
     return render(request, "contact.html", {})
+
+
+def pool_owners(request):
+    transactions = pd.read_csv("calculator/static/POOL_Transactions/POOL_Activity_2017_01_01_to_2018_02_27.csv")
+    owner_lookup = {}
+    # when people get rid of coins, this will subtract from their total
+    for i, j in enumerate(transactions.values):
+        try:
+            owner_lookup[j[4]] = owner_lookup[j[4]] + j[-1] * -1
+        except:
+            owner_lookup[j[4]] = j[-1] * -1
+    # now do the recipients of the coins
+    for i, j in enumerate(transactions.values):
+        try:
+            owner_lookup[j[5]] = owner_lookup[j[5]] + j[-1]
+        except:
+            owner_lookup[j[5]] = j[-1]
+    total_pool = 0
+    for wallet, value in owner_lookup.items():
+        if(wallet != "0x0000000000000000000000000000000000000000" and wallet != '0x009d7bde00c7b4025e4110cd73261d760a349133' ):
+            total_pool += value
+    return render(request, "pool_owners.html", {"coin_holders": owner_lookup, "total_pool": total_pool})
+
+
+def historical_payouts(request):
+    payout_amounts = PayoutAmount.objects.order_by('payout_date').values()
+    return render(request, "historical_payouts.html", {"payout_amounts": payout_amounts})
